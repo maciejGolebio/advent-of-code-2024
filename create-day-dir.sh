007c48e935f69a1cd9de4d138ebcd5d411393fdf
maciejGolebio <czerwoneszelki19@gmail.com>
2024-12-03 08:15:08 +0100

    Script for preparing day structure

---
+++ create-day-dir.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# Create a directory for the current day
+
+# Get the current day of December
+day=$(date +%d)
+
+mkdir -p day$day
+mkdir -p day$day/part1
+mkdir -p day$day/part2
+
+touch day$day/part1/input.txt
+touch day$day/part2/input.txt
+touch day$day/part1/main.py
+touch day$day/part2/main.py
